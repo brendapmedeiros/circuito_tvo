5576893bbd313467f07e99d320230c921aed0b40
Brenda Medeiros <medeirosbre1@gmail.com>
2026-06-02 23:21:21 -0300

    Direcionamento do banco de dados para teste de conexão e deploy.

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 const prisma = require('./database/prisma')
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 8080
 
 async function startServer() {
   try {
